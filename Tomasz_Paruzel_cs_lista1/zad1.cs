adceafaaeb0d54c50588871fb08b1656a14af84a
Oregano01 <tom.krzy.par@gamil.com>
2022-05-10 11:02:05 +0200

    zad1 gotowe

--- Tomasz_Paruzel_cs_lista1/zad1.cs
+++ Tomasz_Paruzel_cs_lista1/zad1.cs
@@ -1,25 +1,27 @@
 ﻿using System;
+using System.Text;
 
 class MultiTable
 {
     public static void Main(string[] args) {
 
         Console.WriteLine("Podaj liczbe naturalną: ");
-        int x = int.Parse(Console.ReadLine());
+        int x = int.Parse(s: Console.ReadLine());
         Console.WriteLine("\n");
         // Console.WriteLine(x);
 
         for (int i=0; i<=x; i++) {
-            Console.Write("{0, 3}", i);
+            Console.Write("{0, 4}", i);
         }
         Console.WriteLine("\n");
 
         for (int i=1; i<=x; i++) {
-            Console.WriteLine("{0, 3}", i);
+            Console.Write("{0, 4}", i);
 
             for (int j=1; j<=x; j++) {
-                Console.Write(String.Format("{0, 3", j));
+                Console.Write("{0, 4}", i*j);
             }
+        Console.WriteLine("\n");
         }
     }
 }
